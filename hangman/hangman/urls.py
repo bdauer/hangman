13bdd1b4cd794c444966ed4945672a7c2e8014c2
bdauer <dauer.benjamin@gmail.com>
2017-02-09 11:51:09 -0500

    Add namespace to hangman_game, import settings, import url and include

--- hangman/hangman/urls.py
+++ hangman/hangman/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hangman/', include('hangman_game.urls')),
+    url(r'^hangman/', include('hangman_game.urls', namespace="hangman_game")),
 ]
 
 if settings.DEBUG:
