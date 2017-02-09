bd4fc91803da2827bde926e49430ef197605172a
bdauer <dauer.benjamin@gmail.com>
2017-02-09 16:53:23 -0500

    On index view access, access userid cookie and renew the expiration. If there is no cookie, create one and create a new UserHistory instance. The cookie is a uuid.

--- hangman/hangman_game/views.py
+++ hangman/hangman_game/views.py
@@ -2,12 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
 from django.core.urlresolvers import reverse_lazy
 from .models import Game, UserHistory, Word
+import uuid
+import datetime
 # Create your views here.
 def index(request):
     """
     The index page is a very basic menu for starting a new game.
     """
-    return render(request, 'hangman_game/index.html')
+    # response = HttpResponse()
+    response = render(request, 'hangman_game/index.html')
+
+    # use for setting cookie expiration
+    today = datetime.date.today()
+    three_months = datetime.timedelta(days=90)
+    three_months_from_today = today + three_months
+
+    try:
+        user_cookie = request.get_signed_cookie('userid')
+        response.set_signed_cookie('userid', user_cookie,
+                                   expires=three_months_from_today)
+    except KeyError:
+        user_cookie = uuid.uuid4()
+        UserHistory.objects.create(user_cookie=user_cookie)
+        response.set_signed_cookie('userid', user_cookie,
+                                   expires=three_months_from_today)
+
+    return response
+    # import ipdb; ipdb.set_trace()
+    # return render(request, 'hangman_game/index.html')
 
 def start_game(request):
 
