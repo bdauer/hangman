dbe3fc2bede555fff0f1356bc5abc19e445504d0
bdauer <dauer.benjamin@gmail.com>
2017-02-09 11:54:47 -0500

    Add basic index view, add placeholder endpoint for starting new game

--- hangman/hangman_game/views.py
+++ hangman/hangman_game/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
+from django.core.urlresolvers import reverse_lazy
 # Create your views here.
+def index(request):
+    """
+    The index page is a very basic menu for starting a new game.
+    """
+    return render(request, 'hangman_game/index.html')
+
+def start_game(request):
+
+    return HttpResponse()
