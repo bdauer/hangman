b1b1385449fd8c1364a01686d326c40fdb051029
bdauer <dauer.benjamin@gmail.com>
2017-02-09 11:48:57 -0500

    Add core app to settings and add static_root for future use

--- hangman/hangman/settings.py
+++ hangman/hangman/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hangman',
     'hangman_game',
 ]
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if DEBUG:
     MIDDLEWARE += (
