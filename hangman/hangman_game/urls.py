7db77166f485ed2ff311279e502ec39bef446034
bdauer <dauer.benjamin@gmail.com>
2017-02-10 13:35:04 -0500

    Add url for game view.

--- hangman/hangman_game/urls.py
+++ hangman/hangman_game/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^start_game', views.start_game, name='start game')
+    url(r'^start_game$', views.start_game, name='start game'),
+    url(r'^game$', views.game, name='game')
 
 ]
