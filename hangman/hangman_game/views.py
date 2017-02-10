4018f24951c76edd71556ed8570d9d0405d0ab21
bdauer <dauer.benjamin@gmail.com>
2017-02-10 13:34:05 -0500

    Create game view, set start_game to redirect to it. Restore last game if it wasn't completed.

--- hangman/hangman_game/views.py
+++ hangman/hangman_game/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
 from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Game, UserHistory, Word
 import uuid
 import datetime
@@ -31,12 +33,46 @@ def index(request):
     # import ipdb; ipdb.set_trace()
     # return render(request, 'hangman_game/index.html')
 
-def start_game(request):
+def game(request):
 
-    userid = request.get_signed_cookie('userid')
+    try:
+        userid = request.get_signed_cookie('userid')
+    except KeyError:
+        redirect('hangman_game:index', permanent=True)
+
+    game = Game.objects.get(pk=request.session['gameid'])
+    return render(request, 'hangman_game/game.html', {'game': game})
+
+def start_game(request):
+    """
+    Start a new game and redirect to the main game page.
+    """
+
+    # If the client doesn't have a cookie for tracking history,
+    # redirect them to the index where they'll get a cookie.
+    try:
+        userid = request.get_signed_cookie('userid')
+    except KeyError:
+        redirect('hangman_game:index', permanent=True)
+
     user_history = UserHistory.objects.get(user_cookie=userid)
 
-    game = Game.objects.create_game()
-    game.user_history = user_history
+    # If there is an active game associated with the client,
+    # restore that game instead of creating a new one.
+    # will need to troubleshoot this once everything is connected.
+    # If there's an active game, the backend needs to
+    # get the character indices for each letter_played
+    # and send it to the frontend.
+    try:
+        game = Game.objects.get(pk=user_history.active_game_id)
+        import ipdb; ipdb.set_trace()
+    except ObjectDoesNotExist:
+        game = Game.objects.create_game(user_history)
 
-    return render(request, 'hangman_game/game.html', {'game': game})
+    game.user_history.active_game_id = game.id
+    game.user_history.save()
+    request.session['gameid'] = game.id
+
+    return redirect('hangman_game:game',
+                    permanent=True)
+    # return render(request, 'hangman_game/game.html', {'game': game})
