b11fff43b2ed72288e283ab503c7f9bd41824b72
bdauer <dauer.benjamin@gmail.com>
2017-02-12 16:19:53 -0500

    Add endpoint for updating the turn

--- hangman/hangman_game/urls.py
+++ hangman/hangman_game/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^start_game$', views.start_game, name='start game'),
-    url(r'^game$', views.game, name='game')
+    url(r'^game$', views.game, name='game'),
+    url(r'^next_turn/$', views.next_turn, name='next turn'),
 
 ]
